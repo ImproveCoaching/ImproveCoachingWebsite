bb9779def880c221c6abf3c1d32621f9f29221ed
Robin van Dommelen <robinvd95@live.nl>
2025-02-18 18:27:15 +0100

    Content updates

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-const coachingContent = document.getElementById('Coaching-Content');
-const adviesContent = document.getElementById('Advies-Content');
-const deskundigContent = document.getElementById('Deskundigheidsbevordering-Content');
+// const coachingContent = document.getElementById('Coaching-Content');
+// const adviesContent = document.getElementById('Advies-Content');
+// const deskundigContent = document.getElementById('Deskundigheidsbevordering-Content');
 
-adviesContent.classList.add('hidden');
-deskundigContent.classList.add('hidden');
+// adviesContent.classList.add('hidden');
+// deskundigContent.classList.add('hidden');
 
-const coachingButton = document.getElementById('Coaching-Button');
-const adviesButton = document.getElementById('Advies-Button');
-const deskundigButton = document.getElementById('Deskundigheidsbevordering-Button');
+// const coachingButton = document.getElementById('Coaching-Button');
+// const adviesButton = document.getElementById('Advies-Button');
+// const deskundigButton = document.getElementById('Deskundigheidsbevordering-Button');
 
-coachingButton.parentElement.classList.add('active');
+// coachingButton.parentElement.classList.add('active');
 
-coachingButton.addEventListener('click', () => {
-    coachingContent.classList.remove('hidden');
-    adviesContent.classList.add('hidden');
-    deskundigContent.classList.add('hidden');
+// coachingButton.addEventListener('click', () => {
+//     coachingContent.classList.remove('hidden');
+//     adviesContent.classList.add('hidden');
+//     deskundigContent.classList.add('hidden');
 
-    coachingButton.parentElement.classList.add('active');
-    adviesButton.parentElement.classList.remove('active');
-    deskundigButton.parentElement.classList.remove('active');
-});
+//     coachingButton.parentElement.classList.add('active');
+//     adviesButton.parentElement.classList.remove('active');
+//     deskundigButton.parentElement.classList.remove('active');
+// });
 
-adviesButton.addEventListener('click', () => {
-    coachingContent.classList.add('hidden');
-    adviesContent.classList.remove('hidden');
-    deskundigContent.classList.add('hidden');
+// adviesButton.addEventListener('click', () => {
+//     coachingContent.classList.add('hidden');
+//     adviesContent.classList.remove('hidden');
+//     deskundigContent.classList.add('hidden');
 
-    coachingButton.parentElement.classList.remove('active');
-    adviesButton.parentElement.classList.add('active');
-    deskundigButton.parentElement.classList.remove('active');
-});
+//     coachingButton.parentElement.classList.remove('active');
+//     adviesButton.parentElement.classList.add('active');
+//     deskundigButton.parentElement.classList.remove('active');
+// });
 
-deskundigButton.addEventListener('click', () => {
-    coachingContent.classList.add('hidden');
-    adviesContent.classList.add('hidden');
-    deskundigContent.classList.remove('hidden');
+// deskundigButton.addEventListener('click', () => {
+//     coachingContent.classList.add('hidden');
+//     adviesContent.classList.add('hidden');
+//     deskundigContent.classList.remove('hidden');
 
-    coachingButton.parentElement.classList.remove('active');
-    adviesButton.parentElement.classList.remove('active');
-    deskundigButton.parentElement.classList.add('active');
-});
+//     coachingButton.parentElement.classList.remove('active');
+//     adviesButton.parentElement.classList.remove('active');
+//     deskundigButton.parentElement.classList.add('active');
+// });
 
 const coachingNav = document.getElementById('Coaching-Nav');
 const adviesNav = document.getElementById('Advies-Nav');
